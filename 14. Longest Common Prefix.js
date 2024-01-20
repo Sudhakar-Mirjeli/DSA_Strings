2c0839e436d129aa82016bbabb6b1f01d3577920
Sudhakar Mirjeli <sudhakar.mirjeli@bentolabs.io>
2024-01-20 23:58:58 +0530

    solved prefix DSA problem

---
+++ 14. Longest Common Prefix.js
@@ -0,0 +1,51 @@
+/* 
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+ 
+
+Constraints:
+
+    1 <= strs.length <= 200
+    0 <= strs[i].length <= 200
+    strs[i] consists of only lowercase English letters.
+
+
+*/
+
+
+`
+*************************************************************************
+*************SOLUTION of 14. Longest Common Prefix*************
+************************************************************************
+`
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+
+    let result = '';
+    for (let i = 0; i < strs[0].length; i++) {
+        if (strs.every(str => str[i] === strs[0][i]))
+            result += strs[0][i]
+            else break
+    }
+    return result
+
+};
